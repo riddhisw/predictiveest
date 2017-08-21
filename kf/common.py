58ce39e4ed773f7d4beee62771982075c8e2f415
riddhisw <riddhi.sw@gmail.com>
2017-08-21 15:52:04 +1000

    updated for quantised msmts - ver0

--- kf/common.py
+++ kf/common.py
@@ -18,7 +18,7 @@ def calc_inst_params(x_hat_time_slice):
 
 
 #@nb.jit(nopython=True)
-def calc_pred(x_hat_series):
+def calc_pred(x_hat_series, quantised='No'):
     
     '''
     Keyword Arguments:
@@ -31,10 +31,14 @@ def calc_pred(x_hat_series):
     '''
     
     series = x_hat_series.shape[2]
-    pred = np.zeros(series)
+    jitter = np.zeros(series)
     for k in xrange(series):
-        pred[k] = np.sum(x_hat_series[::2, 0, k])
-    return pred
+        jitter[k] = np.sum(x_hat_series[::2, 0, k])
+        
+    if quantised =='No':
+        return jitter
+    else:
+        return projected_msmt(jitter)
 
 
 #@nb.jit(nopython=True)
@@ -81,20 +85,30 @@ def propagate_states(a, x_hat, P_hat, oe, numf):
     P_hat_apriori = np.dot(np.dot(a,P_hat),a.T) + Q
 
     return x_hat_apriori, P_hat_apriori, Q
-    
 
-def calc_Kalman_Gain(h, P_hat_apriori, rk):
+
+def calc_z_proj(h, x_hat_apriori):
+    z_proj = np.dot(h, x_hat_apriori)
+    return z_proj
+
+
+def calc_Kalman_Gain(h_, P_hat_apriori, rk, quantised='No', x_hat_apriori=None):
     '''Returns the Kalman gain and scalar S for performing state updates.
     NB: S can be a matrix <=> rk is a matrix, and S_inv = np.linalg.inverse(S) 
     instead of 1.0/S.
     '''
+    
+    if quantised =='No':
+        h = h_ # Linear Measurement  
+    else:
+        h = -1*np.sin(2.0*calc_z_proj(h_, x_hat_apriori))*h_ # Nonlinear Jacobian eval at current time step
+        
     #S = la.multi_dot([h,P_hat_apriori,h.T]) + rk 
     #intermediary = np.dot(P_hat_apriori, h.T)
     #S = np.dot(h, intermediary) + rk 
-    
     #S = np.dot(np.dot(h, P_hat_apriori), h.T) + rk #Agreement between detailed KFs
+    
     S = np.dot(h, np.dot(P_hat_apriori, h.T)) + rk #Agreement between fast KFs, same as linalg.multi_dot for associativity problem
-
     S_inv = 1.0/S # 1.0/S and np.linalg.inv(S) are equivalent when S is rank 1
     
     if not np.isfinite(S_inv).all():
@@ -104,12 +118,31 @@ def calc_Kalman_Gain(h, P_hat_apriori, rk):
     W = np.dot(P_hat_apriori, h.T)*S_inv
     return W, S
 
+def one_shot_msmt(n=1, p=0.5, num_samples=1):
+    return np.random.binomial(n,p,size=num_samples)
+    
+def projected_msmt(jitter):
+    prob_of_msmt = 0.5 + 0.5*np.cos(2.0*jitter)
+    quantised_msmt =[]
+    for item in prob_of_msmt:
+        quantised_msmt.append(one_shot_msmt(p=item))
+    return np.array(quantised_msmt).ravel()
 
-def calc_residuals(h, x_hat_apriori, msmt):
-    z_proj = np.dot(h,x_hat_apriori)
-    return msmt - z_proj
+def calc_residuals(h, x_hat_apriori, msmt, quantised='No'):
+    jitter = calc_z_proj(h, x_hat_apriori)
+    if jitter > np.pi:
+        print('Total jitter is: ', jitter)
+    if quantised == 'No':
+        return msmt - jitter # Linear measurement
+    else:
+        return msmt - projected_msmt(jitter) # non linear msmt
     
 
-    
-
+       # if projected_msmt < 0:
+       #     print("Saturation:", projected_msmt)
+       #     projected_msmt=0.0
+       # elif projected_msmt > 1:
+       #     print("Saturation:", projected_msmt)
+       #     projected_msmt=1.0
+       # quantised_msmt = one_shot_msmt(p=projected_msmt)
     
