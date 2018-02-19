99205fc031dff95c7ecf59f135c9b59bbb823d9c
riddhisw <riddhi.sw@gmail.com>
2018-02-19 16:08:52 +1100

    Redo certain runs for values of R and variation on test_case 24 since these results yeilded no data under action of crlb_analysis_script.py. Bug test 1

---
+++ crlb_paper_v0/crlb_analysis_script_redo.py
@@ -0,0 +1,50 @@
+import sys
+import os
+
+test_case = int(sys.argv[1])
+var_R_ = float(sys.argv[2])
+name_R_ = str(sys.argv[3])
+cflag_ = int(sys.argv[4])
+variation = int(sys.argv[5])
+
+import numpy as np
+
+###############################################################################
+# FILE PATH AND IMPORT INFO
+###############################################################################
+
+sys.path.append('/project/RDS-FSC-QCL_KF-RW/crlb/')
+output_data = '/scratch/RDS-FSC-QCL_KF-RW/CRLB/output_data/' 
+from riccatti_solver.ricatti_recursion import calculate_crlb
+
+###############################################################################
+# DATA PARAMETERS
+###############################################################################
+
+variation_scan=[variation]
+N=2000
+order=101
+
+###############################################################################
+# RUN SCRIPT
+###############################################################################
+
+varlen=len(variation_scan)
+midtrd = np.zeros((varlen, N, order, order))
+cnflp = np.zeros_like(midtrd)
+
+for idx_var in xrange(varlen):
+
+    try:
+        midtrd[idx_var, :, :,:], cnflp[idx_var,:, :,:] = calculate_crlb(output_data, 
+                                                                        variation_scan[idx_var], 
+                                                                        cflag_, name_R_)
+        
+        sv_data = output_data+'CRLB_tc24_flag_'+str(cflag_)+'_R_'+str(name_R_)+'.npz'
+        np.savez(sv_data, midtrd=midtrd, cnflp=cnflp)
+        print(sys.exc_info())
+    
+    except:
+        print(name_R_, idx_var, 'FAILED')
+        print(sys.exc_info())
+
---
+++ crlb_paper_v0/crlb_data_script_redo.py
@@ -0,0 +1,100 @@
+import sys
+import os
+
+test_case = int(sys.argv[1])
+var_R_ = float(sys.argv[2])
+name_R_ = str(sys.argv[3])
+cflag_ = int(sys.argv[4])
+variation = int(sys.argv[5])
+
+add_crlb_noise = 0.0
+if cflag_ == 1:
+    add_crlb_noise = var_R_*1.0
+    
+
+import numpy as np
+
+###############################################################################
+# FILE PATH AND IMPORT INFO
+###############################################################################
+
+sys.path.append('/project/RDS-FSC-QCL_KF-RW/crlb/')
+input_datapath='/scratch/RDS-FSC-QCL_KF-RW/CRLB/input_data/'
+output_data = '/scratch/RDS-FSC-QCL_KF-RW/CRLB/output_data/' 
+
+from qif.common import generate_AR, noisy_z
+from akf.armakf import get_autoreg_model
+from data_tools.load_raw_cluster_data import LoadExperiment
+from riccatti_solver.ricatti_recursion import info_execute
+
+ver=0
+
+###############################################################################
+# DATA PARAMETERS
+###############################################################################
+
+variation_scan=[variation]
+
+# Number of trials in an ensemble 
+runs = 50
+
+# Process noise 
+true_oe = 0.001
+
+# Initial variance for Kalman filter
+p0init = 1000000
+
+# Number of AR values to disregard in the beginning
+burn_in = 500
+
+# Total length of measurement record
+number_of_steps = 2000
+
+###############################################################################
+# RUN SCRIPT
+###############################################################################
+
+for idx_var in xrange(len(variation_scan)):
+
+    experiment = 0
+    experiment = LoadExperiment(test_case, variation_scan[idx_var], 
+                                QKF_load='No',
+                                QKF_path='',
+                                LKFFB_load ='No',
+                                LKFFB_path='',
+                                LSF_load='No',
+                                AKF_load='Yes', 
+                                AKF_path=input_datapath,
+                                GPRP_load='No')
+
+    order = experiment.AKF_weights.shape[0]
+    
+    dynamical_model = get_autoreg_model(order, experiment.AKF_weights) 
+
+    ensemble_crlb = np.zeros((runs, number_of_steps, 101, 101))
+    ensemble_crlb_coinflip = np.zeros((runs, number_of_steps, 101, 101))
+    
+    sv_data = output_data+'tc24_var_'+str(variation_scan[idx_var])+'_flag_'+str(cflag_)+'_R_'+str(name_R_)+'.npz'
+
+    for idx_runs in xrange(runs):
+
+        x_init = np.random.uniform(low=-0.5*np.pi, high=0.5*np.pi, size=order)
+        
+        true_x = generate_AR(x_init, number_of_steps + burn_in, 
+                             experiment.AKF_weights, true_oe)[burn_in:] 
+        noisy_z_ = noisy_z(true_x, add_crlb_noise) 
+
+        ensemble_crlb[idx_runs, :, :, :] = info_execute(dynamical_model,
+                                              true_x, noisy_z_, 
+                                              true_oe, var_R_, p0init, 'classical')
+
+        ensemble_crlb_coinflip[idx_runs, :, :, :] = info_execute(dynamical_model,
+                                              true_x, noisy_z_, 
+                                              true_oe, var_R_, p0init, 'coin')
+
+
+        if idx_runs%3 == 0 or idx_runs == runs-1:
+            np.savez(sv_data, 
+                     ensemble_crlb=ensemble_crlb,
+                     ensemble_crlb_coinflip=ensemble_crlb_coinflip)
+
